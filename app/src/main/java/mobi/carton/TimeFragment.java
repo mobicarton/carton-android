60dc87ed7d361d9098814a512180be1c4797f677
Damien Brun <makowildcat@gmail.com>
2016-03-05 18:04:37 -0500

    Time, Compass and Subtitle extend PageFragment instead of Fragment (support.v4)

--- app/src/main/java/mobi/carton/TimeFragment.java
+++ app/src/main/java/mobi/carton/TimeFragment.java
@@ -2,12 +2,11 @@ package mobi.carton;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TimeFragment extends Fragment {
+public class TimeFragment extends PageFragment {
 
 
     @Override
--- app/src/main/java/mobi/carton/subtitle/SubtitleFragment.java
+++ app/src/main/java/mobi/carton/subtitle/SubtitleFragment.java
@@ -2,16 +2,16 @@ package mobi.carton.subtitle;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import mobi.carton.PageFragment;
 import mobi.carton.R;
 
-public class SubtitleFragment extends Fragment {
+public class SubtitleFragment extends PageFragment {
 
 
     private RecyclerView mRecyclerView;
