4614d98c620f130edac3416f850d9b766408adb9
Damien Brun <makowildcat@gmail.com>
2016-03-06 16:38:37 -0500

    Create OrigamiFragment an populate OrigamiActivity with few of them

--- app/src/main/java/mobi/carton/origami/OrigamiActivity.java
+++ app/src/main/java/mobi/carton/origami/OrigamiActivity.java
@@ -1,8 +1,15 @@
 package mobi.carton.origami;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import mobi.carton.CustomViewPager;
+import mobi.carton.MenuPagerAdapter;
 import mobi.carton.R;
+import mobi.carton.ZoomOutPageTransformer;
 import mobi.carton.library.CartonActivity;
 
 
@@ -15,5 +22,22 @@ public class OrigamiActivity extends CartonActivity {
 
         setDebug();
         setContentView(R.layout.activity_origami);
+
+        List<Fragment> fragments = new ArrayList<>();
+
+        fragments.add(OrigamiFragment.newInstance("Frog", "Andrew Hudson", 17));
+        fragments.add(OrigamiFragment.newInstance("Cranevar", "Andrew Hudson", 26));
+        fragments.add(OrigamiFragment.newInstance("Tulip", "Andrew Hudson", 23));
+        fragments.add(OrigamiFragment.newInstance("Mouse", "Tavin", 14));
+        fragments.add(OrigamiFragment.newInstance("Lily", "Tavin", 25));
+
+        MenuPagerAdapter pagerAdapter = new MenuPagerAdapter(super.getSupportFragmentManager(), fragments);
+
+        CustomViewPager viewPager = (CustomViewPager) super.findViewById(R.id.viewPager_Origami);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        viewPager.setOffscreenPageLimit(3);
+        final float scale = getResources().getDisplayMetrics().density;
+        viewPager.setPageMargin((int) -(40 * scale + 0.5f));
     }
 }
---
+++ app/src/main/java/mobi/carton/origami/OrigamiFragment.java
@@ -0,0 +1,65 @@
+package mobi.carton.origami;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import mobi.carton.R;
+
+public class OrigamiFragment extends Fragment {
+
+
+    private static final String ARG_NAME = "arg_name";
+    private static final String ARG_NB_STEPS = "arg_nb_steps";
+    private static final String ARG_AUTHOR = "arg_author";
+
+
+    public static OrigamiFragment newInstance(String name, String author, int nbSteps) {
+        OrigamiFragment fragment = new OrigamiFragment();
+
+        Bundle args = new Bundle();
+        args.putString(ARG_NAME, name);
+        args.putString(ARG_AUTHOR, author);
+        args.putInt(ARG_NB_STEPS, nbSteps);
+
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+
+    public OrigamiFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_origami, container, false);
+
+        Bundle bundle = getArguments();
+        String name = bundle.getString(ARG_NAME);
+        TextView textViewName = (TextView) rootView.findViewById(R.id.textView_origamiName);
+        textViewName.setText(name);
+
+        TextView textViewAuthor = (TextView) rootView.findViewById(R.id.textView_origamiAuthor);
+        textViewAuthor.setText(bundle.getString(ARG_AUTHOR));
+
+        TextView textViewNbSteps = (TextView) rootView.findViewById(R.id.textView_origamiNbSteps);
+        textViewNbSteps.setText(String.format("%d %s", bundle.getInt(ARG_NB_STEPS), getString(R.string.origami_steps)));
+
+        if (name != null) {
+            int resourceId = getResources().getIdentifier(name.toLowerCase().concat("_finished"), "drawable", getActivity().getPackageName());
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.imageView_origamiFinished);
+            imageView.setImageResource(resourceId);
+        }
+
+        return rootView;
+    }
+}
