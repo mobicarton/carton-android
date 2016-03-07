1d1fcff3f56e671f26d030d29d02b0941277e3d7
Damien Brun <makowildcat@gmail.com>
2016-03-06 19:07:11 -0500

    FIX avoid relaunch activity because onPausePage doesn't occur when leaving with startActivity

--- app/src/main/java/mobi/carton/origami/OrigamiMenuFragment.java
+++ app/src/main/java/mobi/carton/origami/OrigamiMenuFragment.java
@@ -32,6 +32,20 @@ public class OrigamiMenuFragment extends PageFragment
 
 
     @Override
+    public void onPause() {
+        super.onPause();
+        mHeadRecognition.stop();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mHeadRecognition.start();
+    }
+
+
+    @Override
     public void onResumePage() {
         mHeadRecognition.start();
     }
