024f400700e75d1cee90c3b95574349d98ff7599
Damien Brun <makowildcat@gmail.com>
2016-03-11 01:05:07 -0500

    Add phone tracking (using head tracking listener from HeadRecognition) to pass launcher with mirrored screen for Carton

--- library/src/main/java/mobi/carton/library/LauncherActivity.java
+++ library/src/main/java/mobi/carton/library/LauncherActivity.java
@@ -7,7 +7,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
-public class LauncherActivity extends Activity {
+public class LauncherActivity extends Activity
+        implements
+        HeadRecognition.OnHeadTrackingListener {
 
 
     public static final int CODE_LAUNCHER = 32767;
@@ -15,11 +17,31 @@ public class LauncherActivity extends Activity {
     public static final String EXTRA_WITHOUT = "extra_without";
 
 
+    private HeadRecognition mHeadRecognition;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_launcher);
+
+        mHeadRecognition = new HeadRecognition(this);
+        mHeadRecognition.setOnHeadTrackingListener(this);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHeadRecognition.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHeadRecognition.stop();
     }
 
 
@@ -37,5 +59,16 @@ public class LauncherActivity extends Activity {
         intent.putExtra(EXTRA_WITHOUT, true);
         setResult(CODE_LAUNCHER, intent);
         finish();
+    }
+
+
+    @Override
+    public void onDirectionChanged(int azimuth, int pitch, int roll) {
+        if ((pitch < 10 && pitch > -10) && (roll < 10 && roll > - 10)){
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_WITHOUT, false);
+            setResult(CODE_LAUNCHER, intent);
+            finish();
+        }
     }
 }
