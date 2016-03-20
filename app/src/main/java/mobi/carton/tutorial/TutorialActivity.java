3a9755039dff9b201943e3f9d2c3701e119533fb
Damien Brun <makowildcat@gmail.com>
2016-03-20 14:13:44 -0400

    Update TutorialActivity (new step and remove HeadRecognition)

--- app/src/main/java/mobi/carton/tutorial/TutorialActivity.java
+++ app/src/main/java/mobi/carton/tutorial/TutorialActivity.java
@@ -15,14 +15,9 @@ import mobi.carton.ZoomOutPageTransformer;
 import mobi.carton.library.CartonActivity;
 import mobi.carton.library.HeadRecognition;
 
-public class TutorialActivity extends CartonActivity implements
-        HeadRecognition.OnHeadGestureListener,
+public class TutorialActivity extends CartonActivity
+        implements
         CustomViewPager.OnScrollListener {
-
-
-    private CustomViewPager mViewPager;
-
-    private HeadRecognition mHeadRecognition;
 
 
     @Override
@@ -33,69 +28,26 @@ public class TutorialActivity extends CartonActivity implements
 
         List<Fragment> fragments = new ArrayList<>();
 
-        fragments.add(TutorialFragment.newInstance("A simple tutorial to explain how interact. Make a quick tilt with your head on the right to navigate", 0));
-        fragments.add(TutorialFragment.newInstance("You can tilt on the other side too. Actually you can even nod your head down to navigate forward. You can also continue to the right :)", 0));
-        fragments.add(TutorialFragment.newInstance("Almost done. In fact, you can touch the screen with your finger easily, and swipe left, right, forward or backward, just try.", 0));
-        fragments.add(TutorialFragment.newInstance("Finish! Nothing to do here. You can go to the menu with head or finger interactions you've just learned. Congrats!", 0));
+        fragments.add(TutorialFragment.newInstance(getString(R.string.tutorial_step_1), 0));
+        fragments.add(TutorialFragment.newInstance(getString(R.string.tutorial_step_2), 0));
 
         MenuPagerAdapter pagerAdapter = new MenuPagerAdapter(super.getSupportFragmentManager(), fragments);
 
-        mViewPager = (CustomViewPager) super.findViewById(R.id.viewPager_tutorial);
-        mViewPager.setAdapter(pagerAdapter);
-        mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
-        mViewPager.setOffscreenPageLimit(3);
+        CustomViewPager viewPager = (CustomViewPager) super.findViewById(R.id.viewPager_tutorial);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        viewPager.setOffscreenPageLimit(3);
         final float scale = getResources().getDisplayMetrics().density;
-        mViewPager.setPageMargin((int) -(40 * scale + 0.5f));
+        viewPager.setPageMargin((int) -(40 * scale + 0.5f));
 
-        mViewPager.setOnScrollListener(this);
-
-        mHeadRecognition = new HeadRecognition(this);
-        mHeadRecognition.setOnHeadGestureListener(this);
+        viewPager.setOnScrollListener(this);
 
         Utils.setTutorialDone(this, true);
     }
 
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        mHeadRecognition.start();
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mHeadRecognition.stop();
-    }
-
-
-    @Override
-    public void onTilt(int direction) {
-        switch (direction) {
-            case HeadRecognition.TILT_RIGHT:
-                mViewPager.nextPage();
-                break;
-            case HeadRecognition.TILT_LEFT:
-                mViewPager.previousPage();
-                break;
-        }
-    }
-
-
-    @Override
-    public void onNod(int direction) {
-        actionDirection(direction);
-    }
-
-
-    @Override
     public void onScroll(int direction) {
-        actionDirection(direction);
-    }
-
-
-    private void actionDirection(int direction) {
         switch (direction) {
             case HeadRecognition.NOD_DOWN:
                 Intent intent = new Intent(this, TutorialSpeechRecognitionActivity.class);
